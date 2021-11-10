ef6f0f297902ffd13c1f7ba1997fe6442948fa04
Vladislav Rusakov <v.rusakov@q-pol.ru>
2021-11-10 12:40:15 +0300

    Tests running changed

--- tests.py
+++ tests.py
@@ -1,18 +1,31 @@
 from data import get_data_from_weather_api
 
 
-def test_get_data_from_weather_api():
-    assert type(
-        get_data_from_weather_api()) == dict, "Should be a dictionary"
-    print("Data is dictionary. Test 1 passed.")
-
-
 def test_response_get_data_from_weather_api():
     assert get_data_from_weather_api(mode="test") == 200, "Should be 200"
-    print("Status code 200. Test 2 passed.")
+
+
+def test_type_get_data_from_weather_api():
+    assert type(
+        get_data_from_weather_api()) == dict, "Should be a dictionary"
+
+
+def test_content_response_get_data_from_weather_api():
+    assert get_data_from_weather_api()[
+        "openweathermap"] != {}, "Openweathermap should not be empty"
+
+    assert get_data_from_weather_api()[
+        "weatherbit"] != {}, "Weatherbit should not be empty"
+
+
+test_list = [test_response_get_data_from_weather_api,
+             test_type_get_data_from_weather_api,
+             test_content_response_get_data_from_weather_api,
+             ]
 
 
 if __name__ == "__main__":
-    test_get_data_from_weather_api()
-    test_response_get_data_from_weather_api()
+    for index, test in enumerate(test_list):
+        test()
+        print(f"Test {index + 1} passed.")
     print("*******All tests passed*******")
