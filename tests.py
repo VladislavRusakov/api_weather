ea9c917583961523b741db4c2d6e9de1431ac5c9
Vladislav Rusakov <v.rusakov@q-pol.ru>
2021-11-10 15:58:38 +0300

    Added dockerignore

--- tests.py
+++ tests.py
@@ -2,15 +2,18 @@ from data import get_data_from_weather_api
 
 
 def test_response_get_data_from_weather_api():
+    """Тест 1. Проверяет ответ от сервера """
     assert get_data_from_weather_api(mode="test") == 200, "Should be 200"
 
 
 def test_type_get_data_from_weather_api():
+    """"""
     assert type(
         get_data_from_weather_api()) == dict, "Should be a dictionary"
 
 
 def test_content_response_get_data_from_weather_api():
+    """"""
     assert get_data_from_weather_api()[
         "openweathermap"] != {}, "Openweathermap should not be empty"
 
